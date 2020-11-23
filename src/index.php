e31098994c64f15e5160cd7fb37f9841badeaaf2
alvaro alves <alvaroalves87@gmail.com>
2020-11-23 19:44:39 -0300

    started index

--- src/index.php
+++ src/index.php
@@ -5,6 +5,7 @@ include __DIR__. './Config.php';
 <html lang='pt-br'>
     <header>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>teste</title>
         <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossorigin='anonymous' />
         <link href='https://use.fontawesome.com/releases/v5.1.0/css/all.css' rel='stylesheet' integrity='sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt' crossorigin='anonymous' />
@@ -13,30 +14,36 @@ include __DIR__. './Config.php';
         <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossorigin='anonymous'></script>
     </header>
     <body>
-    
-    <?php
+        <div class="container">
+            <nav class="navbar fixed-top navbar-dark bg-primary">
+                <div class="col-8 offset-2">
+                    <a class="btn btn-success float-right">Search</a>
+                </div>
+            </nav>
+        
+            <?php
 
-      $request = $_SERVER['REQUEST_URI'];
-      
-      switch ($request) {
-          case URL.'/' :
-              require __DIR__ . '/controller/MedicoController.php';
-              $controller = new MedicoController();
-              $controller->listar();
-              break;
-          case '' :
-              require __DIR__ . '/views/index.php';
-              break;
-          case '/about' :
-              require __DIR__ . '/views/about.php';
-              break;
-          default:
-            require __DIR__ . '/controller/MedicoController.php';
-            $controller = new MedicoController();
-            $controller->listar();
-            break;
-      }
-    ?>
-
+            $request = $_SERVER['REQUEST_URI'];
+            
+            switch ($request) {
+                case URL.'/' :
+                    require __DIR__ . '/controller/MedicoController.php';
+                    $controller = new MedicoController();
+                    $controller->listar();
+                    break;
+                case '' :
+                    require __DIR__ . '/views/index.php';
+                    break;
+                case '/about' :
+                    require __DIR__ . '/views/about.php';
+                    break;
+                default:
+                    require __DIR__ . '/controller/MedicoController.php';
+                    $controller = new MedicoController();
+                    $controller->listar();
+                    break;
+            }
+            ?>
+        </div>
     </body>
 </html>
--- src/view/index.php
+++ src/view/index.php
@@ -1,27 +1,14 @@
-<main>
-  <div class="container">
-    <div class="row">
-      <div class="col-8 offset-2" style="margin-top:100px">
-
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php 
-            
-            foreach ($data['medicos'] as $medico) { ?>
-            <tr>
-              <td><?= $medico->id ?></td>
-              <td><?= $medico->nome ?></td>
-            </tr>
-            <?php }?>
-          </tbody>
-        </table>
-      </div>
-    </div>
+<div class="col-8 offset-2">
+  <div class="painel">
+  <?php 
+      
+      foreach ($data['medicos'] as $medico) { ?>
+      <tr>
+        <td><?= $medico->id ?></td>
+        <td><?= $medico->nome ?></td>
+      </tr>
+    <?php }?>
+    
   </div>
-</main>
+  
+</div>
